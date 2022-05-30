ba967562846dc464bf6b457f41bb525acf345a3f
unknown <gwanhwikim>
2022-05-30 11:25:08 +0900

    fcm 설정

--- lib/main.dart
+++ lib/main.dart
@@ -111,7 +111,6 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
